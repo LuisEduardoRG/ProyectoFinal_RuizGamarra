60af2da1c3aebc3fbf55604818541b67a19e1c69
Luis Eduardo <luiggis@hotmail.com>
2024-08-01 11:45:30 -0500

    Descripción de los cambios realizados

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -5,11 +5,11 @@ export const Counter = ({ restar, contador, sumar, onAdd }) => {
   return (
     <div className="counter-container">
       <Button variant="contained" className="counter-button" onClick={restar}>
-        Restar
+        Quitar
       </Button>
       <h2>{contador}</h2>
       <Button variant="contained" className="counter-button" onClick={sumar}>
-        Sumar
+        Agregar
       </Button>
       <Button variant="outlined" className="add-to-cart-button" onClick={() => onAdd(contador)}>
         Agregar al carrito
